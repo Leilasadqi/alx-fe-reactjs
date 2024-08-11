1c6f45d33596cae289a805454a76e00b83179b92
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2024-08-11 20:04:23 +0100

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,3 +1,4 @@
+import UserProfile from './components/UserProfile';
 import Header from './components/Header';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
@@ -7,6 +8,7 @@ function App() {
         <div>
             <Header />
             <MainContent />
+            <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
             <Footer />
         </div>
     );
