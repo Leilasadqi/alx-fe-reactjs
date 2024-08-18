a282e641dc56f07b1eea13974e15ab002081b9f4
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2024-08-18 22:02:26 +0100

    Create Footer.jsx

---
+++ alx-react-app-new/Footer.jsx
@@ -0,0 +1,7 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: 'gray', color: 'white', textAlign: 'center', padding: '10px', position: 'fixed', bottom: 0, width: '100%' }}>
+      <p>&copy; 2024 My React App</p>
+    </footer>
+  );
+}
