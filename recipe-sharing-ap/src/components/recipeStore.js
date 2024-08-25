4cdfa1ee97ba7878c0796a9010a62660cf2efd38
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2024-08-25 20:27:39 +0100

    Create recipeStore.js

---
+++ recipe-sharing-ap/src/components/recipeStore.js
@@ -0,0 +1,16 @@
+import create from 'zustand';
+
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  searchTerm: '',
+  filteredRecipes: [],
+  addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setSearchTerm: (term) => set({ searchTerm: term }),
+  filterRecipes: () => set((state) => ({
+    filteredRecipes: state.recipes.filter((recipe) =>
+      recipe.title.toLowerCase().includes(state.searchTerm.toLowerCase())
+    ),
+  })),
+}));
+
+export default useRecipeStore;
