240dbf17136c79e9c83a1be2a66ded95118ac438
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2024-08-25 21:14:58 +0100

    Update recipeStore.js

--- recipe-sharing-app/src/recipeStore.js
+++ recipe-sharing-app/src/recipeStore.js
@@ -5,14 +5,6 @@ const useRecipeStore = create((set) => ({
   searchTerm: '',
   filteredRecipes: [],
   addRecipe: (newRecipe) => set((state) => ({ recipes: [...state.recipes, newRecipe] })),
-  deleteRecipe: (id) => set((state) => ({
-    recipes: state.recipes.filter((recipe) => recipe.id !== id),
-  })),
-  updateRecipe: (id, updatedRecipe) => set((state) => ({
-    recipes: state.recipes.map((recipe) =>
-      recipe.id === id ? { ...recipe, ...updatedRecipe } : recipe
-    ),
-  })),
   setSearchTerm: (term) => set({ searchTerm: term }),
   filterRecipes: () => set((state) => ({
     filteredRecipes: state.recipes.filter((recipe) =>
