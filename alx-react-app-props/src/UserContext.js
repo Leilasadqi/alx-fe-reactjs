52cf3183e684715e9f12c5a57400fd564ea39743
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2024-08-25 22:05:28 +0100

    Update UserContext.js

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,7 +1,5 @@
-import React, { createContext } from 'react';
+import { createContext } from 'react';
 
 const UserContext = createContext();
-
-export const UserProvider = UserContext.Provider;
 
 export default UserContext;
