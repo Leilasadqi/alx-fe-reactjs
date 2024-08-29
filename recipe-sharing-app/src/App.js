88500a2a12820b8714f3b39d53f75d9158f3a73d
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2024-08-29 23:21:21 +0100

    Update App.js

--- recipe-sharing-app/src/App.js
+++ recipe-sharing-app/src/App.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import SearchBar from './SearchBar';
 import RecipeList from './RecipeList';
+import FavoritesList from './FavoritesList';
+import RecommendationsList from './RecommendationsList';
 
 const App = () => {
   return (
@@ -8,6 +10,8 @@ const App = () => {
       <h1>Recipe Sharing App</h1>
       <SearchBar />
       <RecipeList />
+      <FavoritesList />
+      <RecommendationsList />
     </div>
   );
 };
