e75530ee531f1b1dd392345ab463ed3b4d15cba4
Leilasadqi <163025910+Leilasadqi@users.noreply.github.com>
2024-09-15 14:31:04 +0100

    Update App.jsx

--- recipe-sharing-platform/src/App.jsx
+++ recipe-sharing-platform/src/App.jsx
@@ -4,6 +4,23 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import React from 'react';
 import HomePage from './components/HomePage';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import HomePage from './components/HomePage';
+import RecipeDetail from './components/RecipeDetail';
+
+function App() {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/recipe/:id" element={<RecipeDetail />} />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
+
 
 function App() {
   return (
